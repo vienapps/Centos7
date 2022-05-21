a6e5308246dc1243521332d90de83635a4f807bc
Vien Apps Solution <intanharvin@gmail.com>
2022-05-22 06:26:11 +0700

    Add files via upload

---
+++ vien.sh
@@ -0,0 +1,29 @@
+wget http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+rpm -ivh epel-release-latest-7.noarch.rpm
+
+wget https://rpms.remirepo.net/enterprise/remi-release-7.rpm
+rpm -ivh remi-release-7.rpm
+
+yum -y update
+yum -y upgrade
+yum install sudo -y
+yum install -y epel-release
+yum -y install nano
+yum install curl -y
+yum -y install wget
+yum -y install firewalld
+yum -y install openssh-server openssh-clients
+yum install ntp -yyum -y install centos-release-scl
+yum -y install make gcc perl-core pcre-devel wget zlib-devel
+yum makecache
+yum repolist
+yum --disablerepo="*" --enablerepo="epel" list available
+systemctl start sshd.service
+systemctl enable sshd.service
+systemctl start firewalld
+systemctl enable firewalld
+firewall-cmd --reload
+yum clean all
+yum clean dbcache
+rm -rf /var/cache/yum
+rm -rf /tmp/*
