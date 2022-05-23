56a7c8016405258a2825aacdc6ae2538c0f911e8
Vien Apps Solution <intanharvin@gmail.com>
2022-05-23 18:14:06 +0700

    Update vien.sh

--- vien.sh
+++ vien.sh
@@ -5,6 +5,16 @@ if [[ "$USER" != 'root' ]]; then
 	exit
 fi
 
+yum -y install yum-plugin-priorities
+sed -i -e "s/\]$/\]\npriority=1/g" /etc/yum.repos.d/CentOS-Base.repo
+yum -y install epel-release
+sed -i -e "s/\]$/\]\npriority=5/g" /etc/yum.repos.d/epel.repo
+yum -y install centos-release-scl-rh centos-release-scl
+sed -i -e "s/\]$/\]\npriority=10/g" /etc/yum.repos.d/CentOS-SCLo-scl.repo
+sed -i -e "s/\]$/\]\npriority=10/g" /etc/yum.repos.d/CentOS-SCLo-scl-rh.repo
+yum -y install http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
+sed -i -e "s/\]$/\]\npriority=10/g" /etc/yum.repos.d/remi-safe.repo
+
 wget http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 rpm -ivh epel-release-latest-7.noarch.rpm
 
@@ -31,7 +41,16 @@ systemctl start firewalld
 systemctl enable firewalld
 firewall-cmd --reload
 systemctl restart sshd.service
+rm -f epel-release-latest-7.noarch.rpm
+rm -f remi-release-7.rpm
+wget http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+rpm -ivh epel-release-latest-7.noarch.rpm
+wget https://rpms.remirepo.net/enterprise/remi-release-7.rpm
+rpm -ivh remi-release-7.rpm
 yum clean all
 yum clean dbcache
 rm -rf /var/cache/yum
 rm -rf /tmp/*
+rm -f epel-release-latest-7.noarch.rpm
+rm -f remi-release-7.rpm
+rm -f vien.sh
