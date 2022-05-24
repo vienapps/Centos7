47ee59934ab8a462d868eaa0bede198d4ee4d6b7
Vien Apps Solution <intanharvin@gmail.com>
2022-05-24 21:33:15 +0700

    Update vien.sh

--- vien.sh
+++ vien.sh
@@ -6,44 +6,66 @@ if [[ "$USER" != 'root' ]]; then
 fi
 
 wget -O /etc/environment "https://raw.githubusercontent.com/vienapp/Centos7/master/environment"
+sleep 2
 
+echo "###################################################################"
+echo "                		 Install NTP LOKAL                            "
+echo "###################################################################"
+cd
 yum -y install ntp
 wget -O /etc/ntp.conf "https://raw.githubusercontent.com/vienapp/Centos7/master/ntp.conf"
 systemctl enable ntpd && systemctl start ntpd
 ntpq -p
 timedatectl set-timezone Asia/Jakarta
 timedatectl
+sleep 2
 
-export LANG=en_US.UTF-8 && export LANGUAGE=en_US.UTF-8 && export LC_COLLATE=C && export LC_CTYPE=en_US.UTF-8 && yum check
+echo "###################################################################"
+echo "                		 Install Repository                           "
+echo "###################################################################"
+cd
+yum localinstall --nogpgcheck https://raw.githubusercontent.com/vienapp/Centos7/master/rpmfusion-free-release-7.noarch.rpm
+yum localinstall --nogpgcheck https://raw.githubusercontent.com/vienapp/Centos7/master/rpmfusion-nonfree-release-7.noarch.rpm
+yum localinstall --nogpgcheck https://raw.githubusercontent.com/vienapp/Centos7/master/epel-release-7-14.noarch.rpm
+yum localinstall --nogpgcheck https://raw.githubusercontent.com/vienapp/Centos7/master/remi-release-7.9.rpm
+rpm --import https://raw.githubusercontent.com/vienapp/Centos7/master/RPM-GPG-KEY-elrepo.org
+rpm -Uvh https://raw.githubusercontent.com/vienapp/Centos7/master/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
+yum localinstall --nogpgcheck https://raw.githubusercontent.com/vienapp/Centos7/master/webtatic-release.rpm
+sleep 2
 
+yum clean all
+yum update
+yum -y install yum-utils
 yum -y install yum-plugin-fastestmirror
 yum -y install yum-plugin-priorities
-sed -i -e "s/\]$/\]\npriority=1/g" /etc/yum.repos.d/CentOS-Base.repo
 yum -y install epel-release
-sed -i -e "s/\]$/\]\npriority=5/g" /etc/yum.repos.d/epel.repo
 yum -y install centos-release-scl-rh centos-release-scl
+sed -i -e "s/\]$/\]\npriority=1/g" /etc/yum.repos.d/CentOS-Base.repo
+sed -i -e "s/\]$/\]\npriority=5/g" /etc/yum.repos.d/epel.repo
 sed -i -e "s/\]$/\]\npriority=10/g" /etc/yum.repos.d/CentOS-SCLo-scl.repo
 sed -i -e "s/\]$/\]\npriority=10/g" /etc/yum.repos.d/CentOS-SCLo-scl-rh.repo
-yum -y install http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
 sed -i -e "s/\]$/\]\npriority=10/g" /etc/yum.repos.d/remi-safe.repo
-yum -y install centos-release-scl
-yum -y install make gcc perl-core pcre-devel wget zlib-devel
-yum --disablerepo="*" --enablerepo="epel" list available
 
 yum -y update
 yum -y upgrade
+
+echo "###################################################################"
+echo "                				Install Paket                           "
+echo "###################################################################"
+cd
 yum -y install sudo nano curl firewalld openssh-server openssh-clients
 systemctl start sshd.service
 systemctl enable sshd.service
-systemctl start firewalld
-systemctl enable firewalld
+firewall-cmd --permanent --zone=public --add-service=ssh
 firewall-cmd --reload
 systemctl restart sshd.service
-yum makecache
-yum clean all
-yum clean dbcache
+systemctl start firewalld
+systemctl enable firewalld
+systemctl restart firewalld
+
 yum -y update
 yum -y upgrade
-rm -rf vien.sh
+cd
+rm -rf /root/vien.sh
 rm -rf /var/cache/yum
 rm -rf /tmp/*
