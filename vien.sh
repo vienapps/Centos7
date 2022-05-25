9246ed572cbc194ed3b582249d968d77f02890a6
Vien Apps Solution <intanharvin@gmail.com>
2022-05-25 13:36:20 +0700

    Update vien.sh

--- vien.sh
+++ vien.sh
@@ -4,7 +4,7 @@ echo "###################################################################"
 echo "                     MULAI INSTALASI !!                            "
 echo "                    Script By HARVIEN !!                           "
 echo "###################################################################"
-sleep 15
+sleep 10
 
 if [[ "$USER" != 'root' ]]; then
 	echo "Maaf, Anda harus menjalankan ini sebagai root !!!"
@@ -18,11 +18,12 @@ rm -rf /tmp/*
 yum clean all
 
 wget -O /etc/environment "https://raw.githubusercontent.com/vienapp/Centos7/master/environment"
-sleep 2
+sleep 3
 
 echo "###################################################################"
 echo "                      Install NTP LOKAL                            "
 echo "###################################################################"
+sleep 3
 cd
 yum -y install ntp
 wget -O /etc/ntp.conf "https://raw.githubusercontent.com/vienapp/Centos7/master/ntp.conf"
@@ -30,11 +31,11 @@ systemctl enable ntpd && systemctl start ntpd
 ntpq -p
 timedatectl set-timezone Asia/Jakarta
 timedatectl
-sleep 2
 
 echo "###################################################################"
 echo "                      Install Repository                           "
 echo "###################################################################"
+sleep 3
 cd
 yum -y install yum-utils
 yum -y install yum-plugin-fastestmirror
@@ -47,11 +48,16 @@ sed -i -e "s/\]$/\]\npriority=10/g" /etc/yum.repos.d/CentOS-SCLo-scl.repo
 sed -i -e "s/\]$/\]\npriority=10/g" /etc/yum.repos.d/CentOS-SCLo-scl-rh.repo
 sed -i -e "s/\]$/\]\npriority=10/g" /etc/yum.repos.d/remi-safe.repo
 
+echo "###################################################################"
+echo "                        Update Paket                               "
+echo "###################################################################"
+sleep 3
 yum -y update && yum -y upgrade
 
 echo "###################################################################"
 echo "                           Install Paket                           "
 echo "###################################################################"
+sleep 3
 cd
 yum -y install sudo nano curl firewalld openssh-server openssh-clients
 systemctl start sshd.service
@@ -66,7 +72,7 @@ systemctl restart firewalld
 echo "###################################################################"
 echo "                           Pembersihan                             "
 echo "###################################################################"
-sleep 10
+sleep 7
 cd
 rm -rf /root/vien.sh
 rm -rf /var/cache/yum
